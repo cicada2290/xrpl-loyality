44c3c3083451ab10a828e8fa9b3387c0ed0dea63
pokotaro.eth <kentarou_masuda@ruckplus-tech.io>
2025-01-20 01:35:11 +0900

    🎉 Initialize project with config files and smart contracts

---
+++ contracts/loyality.c
@@ -0,0 +1,29 @@
+#include "hookapi.h"
+
+int64_t cbak(uint32_t reserved)
+{
+    TRACESTR("Carbon: callback called.");
+
+    return 0;
+}
+
+int64_t hook(uint32_t reserved ) {
+
+    TRACESTR("loyality.c: Called.");
+
+    // HOOK ON: TT
+    int64_t tt = otxn_type();
+    TRACEVAR(tt);
+
+    // NFTokenMint Tx
+    if (tt == ttNFTOKEN_MINT) {
+        TRACESTR("NFTokenMint Tx");
+    }
+
+    // Payment Tx
+    if (tt == ttPAYMENT) {
+        TRACESTR("Payment Tx");
+    }
+
+    return 0;
+}
